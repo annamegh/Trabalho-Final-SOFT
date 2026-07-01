26ca2caeb6483236585b608505a5999fe84cff7c
Anna Paula Meneghelli de Oliveira <130464385+annamegh@users.noreply.github.com>
2026-07-01 14:46:00 -0300

    Update DataRealizacaoTeste.java
    Add exclusão.

--- testes/DataRealizacaoTeste.java
+++ testes/DataRealizacaoTeste.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import dados.DataRealizacao;
+import dados.Tarefa;
 
 class DataRealizacaoTeste {
 
@@ -25,6 +26,17 @@ class DataRealizacaoTeste {
 		d1.atualizar();
 		
 		assertEquals(d1.getData(), LocalDate.of(2026, 06, 25), "Data de realização da tarefa atualizada com sucesso");
+	}
+	
+	@Test 
+	void testeExcluirDataRealizacao() {
+		Tarefa t1 = new Tarefa(1L, "Limpeza");
+		DataRealizacao d1 = new DataRealizacao(1L, LocalDate.of(2026, 07, 01));
+
+		t1.getDatasRealizacao().add(d1);
+		d1.excluir();
+		
+		assertEquals(0, t1.getDatasRealizacao().size(), "Data de realização da tarefa excluida com sucesso");
 	}
 
 }
